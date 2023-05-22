02518a5b135c42b07ff0feeaf7c3ec3b2156e5c3
FELIPE RAMIREZ LOAIZA <feliperamirez298927@correo.itm.edu.co>
2023-05-21 23:16:33 -0500

    New Entity Ticket and new DataBaseContext

---
+++ ParcialFelipeRamirezLoaiza/ParcialFelipeRamirezLoaiza/DAL/DataBaseContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ParcialFelipeRamirezLoaiza.DAL.Entities;
+
+namespace ParcialFelipeRamirezLoaiza.DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Ticket> Tickets { get; set; }
+    }
+}
---
+++ ParcialFelipeRamirezLoaiza/ParcialFelipeRamirezLoaiza/DAL/Entities/Ticket.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ParcialFelipeRamirezLoaiza.DAL.Entities
+{
+    public class Ticket
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public DateTime? UseDate { get; set; }
+        public bool IsUsed { get; set; }
+        public string? EntranceGate { get; set; }
+    }
+}
